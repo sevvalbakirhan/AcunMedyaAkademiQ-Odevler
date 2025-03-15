01dc71a76f8504b15db883dd16479888934d5aa4
Şevval Bakırhan <sevvalbakirhan@gmail.com>
2025-03-15 03:41:03 +0300

    Erişim Belirleyiciler ve Değişkenler ile Çalışma ödevi eklendi.

---
+++ ErişimBelirleyicilerVeDeğişkenler/ErişimBelirleyicilerVeDeğişkenler/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Ödev İçeriği:
+
+1. Araştırma Kısmı:
+
+⁠ ⁠C# dilinde kullanılan erişim belirleyiciler (public, private, protected, internal) nelerdir? Her birini gerçek hayattan bir örnekle açıklayın.
+Erişim belirleyicilerin nesne yönelimli programlamadaki (OOP) rolü nedir?
+
+Public (Herkese Açık)
+Bu belirleyici, üyelerin herkese açık ve erişilebilir olduğunu ifade eder.örnegin bir şehrin meydanı.
+Private (Özel)
+Bu belirleyici, yalnızca aynı sınıf içindeki kod parçalarına erişim sağlar.öernegin bir hastanenin ameliyat odası.
+Protected (Korunmuş)
+Korunmuş üyeler, yalnızca aynı sınıf ve o sınıfı miras alan alt sınıflar tarafından erişilebilir.örnegin bir ailenin evi.
+Internal (Dahili)
+Internal üyeler, aynı projedeki diğer sınıflar tarafından erişilebilir. Proje dışındaki kodlara kapalıdır.örnegin bir bankanın icindeki diger birimler.
+
+OOPdeki yeri:
+
+Erişim belirleyiciler, OOP'de kapsülleme ve soyutlama gibi temel prensipleri destekler. 
+Kapsülleme ile veriler gizlenir, sadece gereken bilgiler dışarı açılır. 
+Soyutlama ile karmaşıklık azaltılarak, dış dünyaya yalnızca önemli detaylar sunulur. Bu, güvenliği artırır,
+kod düzenini korur ve yeniden kullanılabilirliği sağlar. 
+
+
+2.⁠ ⁠Kodlama Kısmı:
+
+⁠ ⁠Bir “Kullanıcı” (User) sınıfı oluşturun.
+ ⁠User sınıfında aşağıdaki özellikleri tanımlayın:
+ ⁠private string adSoyad (Ad ve Soyad, dışarıdan erişilemez.)
+⁠ ⁠private int yas (Yaş, dışarıdan erişilemez.)
+⁠ ⁠public string Email (E-posta adresi, dışarıdan erişilebilir.)
+⁠ ⁠public void BilgileriGoster() metodu ekleyerek ad, soyad ve yaşı ekrana yazdırın.
+⁠ ⁠Kullanıcı sınıfında adSoyad ve yas değişkenlerine erişmek için getter ve setter metotları oluşturun.
+⁠ ⁠Main metodu içinde bir User nesnesi oluşturun ve değerlerini ayarlayarak ekrana yazdırın.
+*/
+using ErişimBelirleyicilerVeDeğişkenler;
+using System;
+
+User user = new User();
+user.SetAdSoyad("Şevval Bakırhan");
+user.SetYas(21);
+user.Email = "sevvalbakirhan@gmail.com";
+user.BilgileriGoster();
---
+++ ErişimBelirleyicilerVeDeğişkenler/ErişimBelirleyicilerVeDeğişkenler/User.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErişimBelirleyicilerVeDeğişkenler
+{
+    using System;
+
+   public class User
+    {
+        
+        private string adSoyad; 
+        private int yas;
+        public string Email;
+
+        public string GetAdSoyad()
+        {
+            return adSoyad;
+        }
+
+        public void SetAdSoyad(string adSoyadDegeri)
+        {
+            adSoyad = adSoyadDegeri;
+        }
+
+        public int GetYas()
+        {
+            return yas;
+        }
+
+        public void SetYas(int yasDegeri)
+        {
+            yas = yasDegeri;
+        }
+
+       
+        public void BilgileriGoster()
+        {
+            Console.WriteLine("Adı Soyadı: " + adSoyad);
+            Console.WriteLine("Yaşı: " + yas);
+            Console.WriteLine("Email Adresi: "+ Email);
+        }
+    }
+
+
+}
