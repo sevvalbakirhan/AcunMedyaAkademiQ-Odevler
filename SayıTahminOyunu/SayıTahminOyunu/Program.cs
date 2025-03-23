7a00217a812ad782dffe46f4154033cf56cff80a
Şevval Bakırhan <sevvalbakirhan@gmail.com>
2025-03-23 21:09:29 +0300

    Sayı Tahmin Oyunu Ödevi Eklendi.

---
+++ SayıTahminOyunu/SayıTahminOyunu/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*Ödev: Bilgisayarın rastgele tuttuğu bir sayıyı tahmin etmeye çalışan bir oyun yazın.
+
+Yapılması Gerekenler:
+
+Bilgisayarın 1 ile 100 arasında rastgele bir sayı üretmesini sağlayın.
+Kullanıcıdan sayı tahmini alarak, büyük/küçük olduğunu belirtin.
+Kullanıcı doğru tahminde bulunana kadar oyunu devam ettirin.
+Tahmin sayısını ekrana yazdırın.*/
+
+using System;
+Console.WriteLine("Bir sayı tutunuz(1-100)");
+int sayi = new Random().Next(1, 101); //C#'ta rastgele bir sayı üretmek için sıkça kullanılan bir yöntemdir. Bu kod,  sınıfını kullanarak 1 ile 100 (dahil) arasında rastgele bir tamsayı oluşturur. new random() Yeni bir rastgele sayı oluşturucu (Random nesnesi) yaratır.
+int tahmin = 0;
+int tahminSayisi = 0;
+while (tahmin != sayi)
+{
+    tahmin = Convert.ToInt32(Console.ReadLine());
+    tahminSayisi++;
+    if (tahmin < sayi)
+    {
+        Console.WriteLine("Daha büyük bir sayı giriniz");
+    }
+    else if (tahmin > sayi)
+    {
+        Console.WriteLine("Daha küçük bir sayı giriniz");
+    }
+    else
+    {
+        Console.WriteLine("Tebrikler " + tahminSayisi + "tahminde doğru sayıyı buldunuz");
+    }
+}
